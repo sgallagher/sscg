60a8c6e5209a072501025eb31bfd003c5d444fcc
Stephen Gallagher <sgallagh@redhat.com>
2021-01-29 09:52:00 -0500

    Switch CI to Github Actions
    Signed-off-by: Stephen Gallagher <sgallagh@redhat.com>

--- .ci/coverity_prep.sh
+++ .ci/coverity_prep.sh
@@ -2,9 +2,9 @@
 
 set -x
 
-echo -n | openssl s_client -connect scan.coverity.com:443 | sed -ne '/-BEGIN CERTIFICATE-/,/-END CERTIFICATE-/p' | sudo tee -a /etc/ssl/certs/ca-
+echo -n | openssl s_client -connect scan.coverity.com:443 | sed -ne '/-BEGIN CERTIFICATE-/,/-END CERTIFICATE-/p' | tee -a /etc/ssl/certs/ca-
 curl -s -L https://entrust.com/root-certificates/entrust_l1k.cer | tee -a /etc/pki/ca-trust/source/anchors/entrust_l1k.cer
-sudo update-ca-trust
+update-ca-trust
 
 
 curl -s https://scan.coverity.com/scripts/travisci_build_coverity_scan.sh -o /usr/bin/travisci_build_coverity_scan.sh
@@ -12,7 +12,6 @@ curl -s https://scan.coverity.com/scripts/travisci_build_coverity_scan.sh -o /us
 if [ "$(file -b --mime-type /usr/bin/travisci_build_coverity_scan.sh)" == "text/x-shellscript" ]; then
     chmod a+x /usr/bin/travisci_build_coverity_scan.sh
 else
-    echo "Warning: Coverity not detected!"
-    # Disable the scanning tool
-    ln -sf /usr/bin/true /usr/bin/travisci_build_coverity_scan.sh
+    echo "Coverity not detected!"
+    exit 1
 fi
