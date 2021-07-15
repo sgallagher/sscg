e268f01ac718d1a95bd5bc8649fee71f69bfeade
Stephen Gallagher <sgallagh@redhat.com>
2021-07-15 16:37:49 -0400

    Add support for named DH parameters
    Fixes https://github.com/sgallagher/sscg/issues/19
    
    Signed-off-by: Stephen Gallagher <sgallagh@redhat.com>

--- src/dhparams.c
+++ src/dhparams.c
@@ -18,6 +18,7 @@
 */
 
 #include <assert.h>
+#include <string.h>
 
 #include <openssl/evp.h>
 
@@ -25,6 +26,45 @@
 #include "include/sscg.h"
 #include "include/dhparams.h"
 
+// clang-format off
+
+#if OPENSSL_VERSION_NUMBER
+const char *dh_fips_groups[] = {
+  "ffdhe2048",
+  "ffdhe3072",
+  "ffdhe4096",
+  "ffdhe6144",
+  "ffdhe8192",
+  NULL,
+};
+
+const char *dh_nonfips_groups[] = {
+  NULL
+};
+#else //OPENSSL_VERSION_NUMBER
+const char *dh_fips_groups[] = {
+  "ffdhe2048",
+  "ffdhe3072",
+  "ffdhe4096",
+  "ffdhe6144",
+  "ffdhe8192",
+  "modp_2048",
+  "modp_3072",
+  "modp_4096",
+  "modp_6144",
+  "modp_8192",
+  NULL,
+};
+
+const char *dh_nonfips_groups[] = {
+  "modp_1536",
+  "dh_1024_160",
+  "dh_2048_224",
+  "dh_2048_256",
+  NULL
+};
+#endif //OPENSSL_VERSION_NUMBER
+// clang-format on
 
 static int
 evp_cb (EVP_PKEY_CTX *ctx);
@@ -137,3 +177,158 @@ evp_cb (EVP_PKEY_CTX *ctx)
 
   return 1;
 }
+
+
+static bool
+is_valid_named_group (const char *group_name)
+{
+  size_t i = 0;
+
+  /* Check FIPS groups */
+  while (dh_fips_groups[i])
+    {
+      if (strcmp (dh_fips_groups[i], group_name) == 0)
+        return true;
+      i++;
+    }
+
+  /* Check non-FIPS groups */
+  if (!FIPS_mode ())
+    {
+      i = 0;
+      while (dh_nonfips_groups[i])
+        {
+          if (strcmp (dh_nonfips_groups[i], group_name) == 0)
+            return true;
+          i++;
+        }
+    }
+
+  return false;
+}
+
+
+#ifdef HAVE_OSSL_PARAM
+int
+get_params_by_named_group (const char *group_name, EVP_PKEY **dhparams)
+{
+  int ret;
+  EVP_PKEY_CTX *pctx = EVP_PKEY_CTX_new_from_name (NULL, "DH", NULL);
+  OSSL_PARAM ossl_params[2];
+  EVP_PKEY *params = NULL;
+  char *name = NULL;
+
+  if (!is_valid_named_group (group_name))
+    {
+      fprintf (stderr, "Unknown Diffie Hellman finite field group.\n");
+      ret = EINVAL;
+      goto done;
+    }
+
+  name = talloc_strdup (NULL, group_name);
+
+  ossl_params[0] = OSSL_PARAM_construct_utf8_string ("group", name, 0);
+  ossl_params[1] = OSSL_PARAM_construct_end ();
+
+  if (!EVP_PKEY_keygen_init (pctx))
+    {
+      ERR_print_errors_fp (stderr);
+      ret = EIO;
+      goto done;
+    }
+
+  if (!EVP_PKEY_CTX_set_params (pctx, ossl_params))
+    {
+      ERR_print_errors_fp (stderr);
+      ret = EIO;
+      goto done;
+    }
+
+  if (!EVP_PKEY_generate (pctx, &params))
+    {
+      ERR_print_errors_fp (stderr);
+      ret = EIO;
+      goto done;
+    }
+
+  *dhparams = params;
+  params = NULL;
+
+  ret = EOK;
+
+done:
+  EVP_PKEY_free (params);
+  EVP_PKEY_CTX_free (pctx);
+  talloc_free (name);
+  return ret;
+}
+
+#else //HAVE_OSSL_PARAM
+
+static int
+get_group_nid (const char *group_name)
+{
+  if (strcmp ("ffdhe2048", group_name) == 0)
+    {
+      return NID_ffdhe2048;
+    }
+  else if (strcmp ("ffdhe3072", group_name) == 0)
+    {
+      return NID_ffdhe3072;
+    }
+  else if (strcmp ("ffdhe4096", group_name) == 0)
+    {
+      return NID_ffdhe4096;
+    }
+  else if (strcmp ("ffdhe6144", group_name) == 0)
+    {
+      return NID_ffdhe6144;
+    }
+  else if (strcmp ("ffdhe8192", group_name) == 0)
+    {
+      return NID_ffdhe8192;
+    }
+  return NID_undef;
+}
+
+int
+get_params_by_named_group (const char *group_name, EVP_PKEY **dhparams)
+{
+  int ret, sslret;
+  DH *dh = NULL;
+  EVP_PKEY *pkey = NULL;
+
+  if (!is_valid_named_group (group_name))
+    {
+      fprintf (stderr, "Unknown Diffie Hellman finite field group.\n");
+      ret = EINVAL;
+      goto done;
+    }
+
+  dh = DH_new_by_nid (get_group_nid (group_name));
+  if (!dh)
+    {
+      fprintf (stderr, "Could not retrieve DH group %s\n", group_name);
+      ret = EINVAL;
+      goto done;
+    }
+
+  pkey = EVP_PKEY_new ();
+  sslret = EVP_PKEY_assign_DH (pkey, dh);
+  CHECK_SSL (sslret, "EVP_PKEY_ASSIGN_DH");
+
+  /* The dhparams are owned by the pkey now */
+  dh = NULL;
+
+  *dhparams = pkey;
+  pkey = NULL;
+
+  ret = EOK;
+
+done:
+  DH_free (dh);
+  EVP_PKEY_free (pkey);
+  return ret;
+}
+
+#endif //HAVE_OSSL_PARAM
---
+++ test/named_dhparams_test.c
@@ -0,0 +1,146 @@
+/*
+    This file is part of sscg.
+
+    sscg is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    sscg is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with sscg.  If not, see <http://www.gnu.org/licenses/>.
+
+    Copyright 2019 by Stephen Gallagher <sgallagh@redhat.com>
+*/
+
+#include <errno.h>
+#include <stdio.h>
+#include <string.h>
+#include <talloc.h>
+#include <openssl/err.h>
+#include <openssl/evp.h>
+
+#include "include/sscg.h"
+#include "include/dhparams.h"
+
+
+static int
+test_valid_named_groups (void)
+{
+  int ret;
+  size_t i = 0;
+  EVP_PKEY *dhparams = NULL;
+  EVP_PKEY_CTX *pctx = NULL;
+
+  if (getenv ("SSCG_SKIP_DHPARAMS"))
+    {
+      /* Skip this test */
+      return 77;
+    }
+
+  while (dh_fips_groups[i])
+    {
+      printf("Testing %s\n", dh_fips_groups[i]);
+      ret = get_params_by_named_group (dh_fips_groups[i], &dhparams);
+      if (ret != EOK) {
+        fprintf (stderr,
+                 "Could not retrieve named DH parameters.");
+        goto done;
+      }
+
+      pctx = EVP_PKEY_CTX_new(dhparams, NULL);
+      if (!EVP_PKEY_param_check(pctx))
+        {
+          ERR_print_errors_fp (stderr);
+          ret = EIO;
+          goto done;
+        }
+
+      i++;
+
+      EVP_PKEY_CTX_free (pctx);
+      pctx = NULL;
+
+      EVP_PKEY_free (dhparams);
+      dhparams = NULL;
+    }
+
+  ret = EOK;
+
+done:
+  EVP_PKEY_free (dhparams);
+  EVP_PKEY_CTX_free (pctx);
+  return ret;
+}
+
+
+static int
+test_invalid_named_groups (void)
+{
+  int ret;
+  EVP_PKEY *dhparams = NULL;
+  TALLOC_CTX *tmp_ctx = NULL;
+  char *name = NULL;
+
+  if (getenv ("SSCG_SKIP_DHPARAMS"))
+    {
+      /* Skip this test */
+      return 77;
+    }
+
+  tmp_ctx = talloc_new (NULL);
+
+  printf("Testing empty string\n");
+  ret = get_params_by_named_group ("", &dhparams);
+  if (ret != EINVAL) {
+    fprintf (stderr,
+             "Received [%s] return code.", strerror(ret));
+    ret = EINVAL;
+    goto done;
+  }
+
+
+  printf("Testing long, unterminated string\n");
+  name = talloc_array (tmp_ctx, char, 10 * 1024 * 1024 + 1);
+  memset (name, 'a', 10 * 1024 * 1024);
+  ret = get_params_by_named_group (name, &dhparams);
+  if (ret != EINVAL) {
+    fprintf (stderr,
+             "Received [%s] return code.", strerror(ret));
+    ret = EINVAL;
+    goto done;
+  }
+  talloc_zfree (name);
+
+
+  EVP_PKEY_free (dhparams);
+  dhparams = NULL;
+
+  ret = EOK;
+
+done:
+  EVP_PKEY_free (dhparams);
+  return ret;
+}
+
+
+int
+main (int argc, char **argv)
+{
+  int ret = EOK;
+
+  ret = test_valid_named_groups ();
+  if (ret != EOK) goto done;
+
+  ret = test_invalid_named_groups ();
+  if (ret != EOK) goto done;
+
+  ret = EOK;
+
+done:
+  return ret;
+}
