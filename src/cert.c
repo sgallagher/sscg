0afc7486116b7ade7084d47aff6018598b15f522
Stephen Gallagher <sgallagh@redhat.com>
2025-11-21 12:54:48 -0500

    Fix incorrect ternary operator
    Now correctly checks against the `type` variable instead of treating it
    as always being a service certificate.
    
    Signed-off-by: Stephen Gallagher <sgallagh@redhat.com>

--- src/cert.c
+++ src/cert.c
@@ -159,8 +159,9 @@ create_cert (TALLOC_CTX *mem_ctx,
 
   if (options->verbosity >= SSCG_DEBUG)
     {
-      const char *tempcert = SSCG_CERT_TYPE_SERVER ? "/tmp/debug-service.csr" :
-                                                     "/tmp/debug-client.csr";
+      const char *tempcert = (type == SSCG_CERT_TYPE_SERVER) ?
+                               "/tmp/debug-service.csr" :
+                               "/tmp/debug-client.csr";
 
       fprintf (stderr, "DEBUG: Writing certificate CSR to %s\n", tempcert);
       BIO *csr_out = BIO_new_file (tempcert, "w");
