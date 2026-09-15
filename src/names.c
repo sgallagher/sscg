ae5e05a5c4641d59fcf5b18a2af5cf6f1d2a9cad
Stephen Gallagher <sgallagh@redhat.com>
2026-09-15 16:31:14 -0400

    Perform input validation on hostnames and SANs
    Input is generally trusted, but this should help protect against user
    mistakes.
    
    Signed-off-by: Stephen Gallagher <sgallagh@redhat.com>

---
+++ src/names.c
@@ -0,0 +1,270 @@
+/* SPDX-License-Identifier: GPL-3.0-or-later WITH cryptsetup-OpenSSL-exception */
+/*
+    Copyright 2025 by Stephen Gallagher <sgallagh@redhat.com>
+*/
+
+#include <arpa/inet.h>
+#include <ctype.h>
+#include <errno.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <string.h>
+
+#include "config.h"
+#ifdef HAVE_GETTEXT
+#include <libintl.h>
+#endif
+
+#include "include/names.h"
+#include "include/sscg.h"
+
+static bool
+has_injection_chars (const char *s, bool forbid_colon)
+{
+  for (const unsigned char *p = (const unsigned char *)s; *p; p++)
+    {
+      if (*p == ',' || *p == ';' || *p == '@' || *p == '\n' || *p == '\r'
+          || isspace (*p) || *p > 0x7f)
+        {
+          return true;
+        }
+      if (forbid_colon && *p == ':')
+        {
+          return true;
+        }
+    }
+  return false;
+}
+
+static bool
+is_ldh_char (char c)
+{
+  return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')
+         || (c >= '0' && c <= '9') || c == '-';
+}
+
+static int
+validate_dns_label (const char *label, size_t len)
+{
+  size_t i;
+
+  if (len == 0 || len > MAX_HOST_LEN)
+    {
+      return EINVAL;
+    }
+
+  if (label[0] == '-' || label[len - 1] == '-')
+    {
+      return EINVAL;
+    }
+
+  for (i = 0; i < len; i++)
+    {
+      if (!is_ldh_char (label[i]))
+        {
+          return EINVAL;
+        }
+    }
+
+  return EOK;
+}
+
+int
+sscg_validate_dns_hostname (const char *name)
+{
+  const char *dot_pos;
+  size_t label_len;
+  const char *start;
+  const char *end;
+
+  if (!name || name[0] == '\0')
+    {
+      fprintf (stderr, _ ("Invalid hostname.\n"));
+      return EINVAL;
+    }
+
+  if (has_injection_chars (name, true))
+    {
+      fprintf (stderr, _ ("Invalid hostname.\n"));
+      return EINVAL;
+    }
+
+  if (strnlen (name, MAX_FQDN_LEN + 1) > MAX_FQDN_LEN)
+    {
+      fprintf (
+        stderr, _ ("FQDNs may not exceed %d characters\n"), MAX_FQDN_LEN);
+      return EINVAL;
+    }
+
+  dot_pos = strchr (name, '.');
+  if (dot_pos)
+    {
+      label_len = (size_t)(dot_pos - name);
+    }
+  else
+    {
+      label_len = strnlen (name, MAX_HOST_LEN + 1);
+    }
+
+  if (label_len > MAX_HOST_LEN)
+    {
+      fprintf (stderr,
+               _ ("Hostname labels may not exceed %d characters\n"),
+               MAX_HOST_LEN);
+      return EINVAL;
+    }
+
+  if (name[0] == '.' || name[strlen (name) - 1] == '.')
+    {
+      fprintf (stderr, _ ("Invalid hostname.\n"));
+      return EINVAL;
+    }
+
+  start = name;
+  while (*start)
+    {
+      end = strchr (start, '.');
+      if (!end)
+        {
+          end = start + strlen (start);
+        }
+
+      label_len = (size_t)(end - start);
+      if (validate_dns_label (start, label_len) != EOK)
+        {
+          if (label_len > MAX_HOST_LEN)
+            {
+              fprintf (stderr,
+                       _ ("Hostname labels may not exceed %d characters\n"),
+                       MAX_HOST_LEN);
+            }
+          else
+            {
+              fprintf (stderr, _ ("Invalid hostname.\n"));
+            }
+          return EINVAL;
+        }
+
+      if (*end == '\0')
+        {
+          break;
+        }
+      start = end + 1;
+      if (*start == '\0')
+        {
+          fprintf (stderr, _ ("Invalid hostname.\n"));
+          return EINVAL;
+        }
+    }
+
+  return EOK;
+}
+
+static int
+validate_ip_san (const char *value)
+{
+  char hostbuf[INET6_ADDRSTRLEN + 1];
+  const char *slash;
+  size_t hostlen;
+
+  slash = strchr (value, '/');
+  if (slash)
+    {
+      hostlen = (size_t)(slash - value);
+      if (hostlen == 0 || hostlen >= sizeof (hostbuf))
+        {
+          fprintf (stderr, _ ("Invalid subject alternative name.\n"));
+          return EINVAL;
+        }
+      memcpy (hostbuf, value, hostlen);
+      hostbuf[hostlen] = '\0';
+
+      for (const char *p = slash + 1; *p; p++)
+        {
+          if (!isdigit ((unsigned char)*p))
+            {
+              fprintf (stderr, _ ("Invalid subject alternative name.\n"));
+              return EINVAL;
+            }
+        }
+    }
+  else
+    {
+      if (strlen (value) >= sizeof (hostbuf))
+        {
+          fprintf (stderr, _ ("Invalid subject alternative name.\n"));
+          return EINVAL;
+        }
+      memcpy (hostbuf, value, strlen (value) + 1);
+    }
+
+  if (strchr (hostbuf, ':'))
+    {
+      struct in6_addr addr6;
+
+      if (inet_pton (AF_INET6, hostbuf, &addr6) != 1)
+        {
+          fprintf (stderr, _ ("Invalid subject alternative name.\n"));
+          return EINVAL;
+        }
+    }
+  else
+    {
+      struct in_addr addr4;
+
+      if (inet_pton (AF_INET, hostbuf, &addr4) != 1)
+        {
+          fprintf (stderr, _ ("Invalid subject alternative name.\n"));
+          return EINVAL;
+        }
+    }
+
+  return EOK;
+}
+
+int
+sscg_validate_subject_alt_name (const char *san)
+{
+  const char *colon;
+
+  if (!san || san[0] == '\0')
+    {
+      fprintf (stderr, _ ("Invalid subject alternative name.\n"));
+      return EINVAL;
+    }
+
+  if (has_injection_chars (san, false))
+    {
+      fprintf (stderr, _ ("Invalid subject alternative name.\n"));
+      return EINVAL;
+    }
+
+  colon = strchr (san, ':');
+  if (!colon)
+    {
+      return sscg_validate_dns_hostname (san);
+    }
+
+  if (strncmp (san, "DNS:", 4) == 0)
+    {
+      if (san[4] == '\0')
+        {
+          fprintf (stderr, _ ("Invalid subject alternative name.\n"));
+          return EINVAL;
+        }
+      return sscg_validate_dns_hostname (san + 4);
+    }
+
+  if (strncmp (san, "IP:", 3) == 0)
+    {
+      if (san[3] == '\0')
+        {
+          fprintf (stderr, _ ("Invalid subject alternative name.\n"));
+          return EINVAL;
+        }
+      return validate_ip_san (san + 3);
+    }
+
+  fprintf (stderr, _ ("Invalid subject alternative name.\n"));
+  return EINVAL;
+}
---
+++ test/dns_name_validation_test.c
@@ -0,0 +1,153 @@
+/* SPDX-License-Identifier: GPL-3.0-or-later WITH cryptsetup-OpenSSL-exception */
+/*
+    Copyright 2025 by Stephen Gallagher <sgallagh@redhat.com>
+*/
+
+#include <errno.h>
+#include <stdio.h>
+#include <string.h>
+#include <talloc.h>
+
+#include "include/names.h"
+#include "include/sscg.h"
+
+struct dns_case
+{
+  const char *name;
+  int expected;
+};
+
+static int
+run_dns_cases (const struct dns_case *cases, size_t n, const char *suite)
+{
+  size_t i;
+  int ret = EOK;
+
+  printf ("%s\n", suite);
+  for (i = 0; i < n; i++)
+    {
+      int got = sscg_validate_dns_hostname (cases[i].name);
+
+      printf ("  [%s] ", cases[i].name ? cases[i].name : "(null)");
+      if (got != cases[i].expected)
+        {
+          printf ("FAILED (got %d, expected %d).\n", got, cases[i].expected);
+          ret = EINVAL;
+        }
+      else
+        {
+          printf ("ok.\n");
+        }
+    }
+  printf ("\n");
+  return ret;
+}
+
+static int
+run_san_cases (const struct dns_case *cases, size_t n, const char *suite)
+{
+  size_t i;
+  int ret = EOK;
+
+  printf ("%s\n", suite);
+  for (i = 0; i < n; i++)
+    {
+      int got = sscg_validate_subject_alt_name (cases[i].name);
+
+      printf ("  [%s] ", cases[i].name);
+      if (got != cases[i].expected)
+        {
+          printf ("FAILED (got %d, expected %d).\n", got, cases[i].expected);
+          ret = EINVAL;
+        }
+      else
+        {
+          printf ("ok.\n");
+        }
+    }
+  printf ("\n");
+  return ret;
+}
+
+int
+main (int argc, char **argv)
+{
+  int ret = EOK;
+  char label_63[64];
+  char fqdn_63[80];
+  char label_64[80];
+
+  talloc_enable_leak_report_full ();
+
+  printf ("=== DNS name validation unit test ===\n\n");
+
+  memset (label_63, 'a', 63);
+  label_63[63] = '\0';
+  snprintf (fqdn_63, sizeof (fqdn_63), "%s.example.com", label_63);
+
+  {
+    char long_label[64 + 1];
+
+    memset (long_label, 'a', 64);
+    long_label[64] = '\0';
+    snprintf (label_64, sizeof (label_64), "%s.example.com", long_label);
+  }
+
+  const struct dns_case accept_dns[] = {
+    { "server.example.com", EOK },
+    { "localhost", EOK },
+    { "a", EOK },
+    { fqdn_63, EOK },
+  };
+
+  const struct dns_case reject_dns[] = {
+    { "a, DNS:evil.com", EINVAL }, { "foo;bar", EINVAL },
+    { "bad_label", EINVAL },       { "..example.com", EINVAL },
+    { ".example.com", EINVAL },    { "example.com.", EINVAL },
+    { label_64, EINVAL },          { "", EINVAL },
+    { "host:name", EINVAL },
+  };
+
+  ret = run_dns_cases (accept_dns,
+                       sizeof (accept_dns) / sizeof (accept_dns[0]),
+                       "sscg_validate_dns_hostname (accept)");
+  if (ret != EOK)
+    {
+      return ret;
+    }
+
+  ret = run_dns_cases (reject_dns,
+                       sizeof (reject_dns) / sizeof (reject_dns[0]),
+                       "sscg_validate_dns_hostname (reject)");
+  if (ret != EOK)
+    {
+      return ret;
+    }
+
+  const struct dns_case accept_san[] = {
+    { "alt.example.com", EOK },   { "DNS:alt.example.com", EOK },
+    { "IP:192.0.2.1", EOK },      { "IP:2001:db8::1", EOK },
+    { "IP:203.0.113.0/24", EOK },
+  };
+
+  const struct dns_case reject_san[] = {
+    { "DNS:evil.com, DNS:other", EINVAL },
+    { "URI:https://example.com", EINVAL },
+    { "OTHER:foo", EINVAL },
+    { "IP:not-an-ip", EINVAL },
+    { "evil, DNS:other", EINVAL },
+  };
+
+  ret = run_san_cases (accept_san,
+                       sizeof (accept_san) / sizeof (accept_san[0]),
+                       "sscg_validate_subject_alt_name (accept)");
+  if (ret != EOK)
+    {
+      return ret;
+    }
+
+  ret = run_san_cases (reject_san,
+                       sizeof (reject_san) / sizeof (reject_san[0]),
+                       "sscg_validate_subject_alt_name (reject)");
+  return ret;
+}
