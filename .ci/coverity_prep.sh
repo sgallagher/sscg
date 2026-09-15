2c607ac30b5135d3154414fed44d1b6d48c00bed
Stephen Gallagher <sgallagh@redhat.com>
2026-09-15 14:40:22 -0400

    CI: use trusted certificates for Coverity scanning
    Signed-off-by: Stephen Gallagher <sgallagh@redhat.com>

--- .ci/coverity_prep.sh
+++ .ci/coverity_prep.sh
@@ -1,16 +1,19 @@
 #!/usr/bin/bash
 
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 set -x
 
-echo -n | openssl s_client -connect scan.coverity.com:443 | sed -ne '/-BEGIN CERTIFICATE-/,/-END CERTIFICATE-/p' | tee -a /etc/ssl/certs/ca-
-curl -s -L https://entrust.com/root-certificates/entrust_l1k.cer | tee -a /etc/pki/ca-trust/source/anchors/entrust_l1k.cer
-update-ca-trust
+# If we are running in a GitHub Actions workflow, use the workspace directory
+# otherwise use the script directory
+workdir=${GITHUB_WORKSPACE:-${SCRIPT_DIR}}
+mkdir -p ${workdir}/coverity
 
+cd ${workdir}/coverity
+curl -s https://scan.coverity.com/scripts/travisci_build_coverity_scan.sh -o travisci_build_coverity_scan.sh
 
-curl -s https://scan.coverity.com/scripts/travisci_build_coverity_scan.sh -o /usr/bin/travisci_build_coverity_scan.sh
-
-if [ "$(file -b --mime-type /usr/bin/travisci_build_coverity_scan.sh)" == "text/x-shellscript" ]; then
-    chmod a+x /usr/bin/travisci_build_coverity_scan.sh
+if [ "$(file -b --mime-type ${workdir}/coverity/travisci_build_coverity_scan.sh)" == "text/x-shellscript" ]; then
+    chmod a+x ${workdir}/coverity/travisci_build_coverity_scan.sh
 else
     echo "Coverity not detected!"
     exit 1
